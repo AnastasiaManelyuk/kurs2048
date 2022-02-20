606c0120aaf99b55c6a46144b3cb36973cdc0615
manlyuk <manlyuk@lanit.ru>
2022-02-20 23:17:54 +0300

    2

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,14 @@
-const CracoLessPlugin = require("craco-less");
+import CracoLessPlugin from "craco-less";
 
-module.exports = {
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-          },
+export const plugins = [
+  {
+    plugin: CracoLessPlugin,
+    options: {
+      lessLoaderOptions: {
+        lessOptions: {
+          javascriptEnabled: true,
         },
       },
     },
-  ],
-};
+  },
+];
